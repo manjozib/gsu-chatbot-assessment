1af434ecd8fee458ea8633d5a2c312f244241215
manjozib <manjozib@gmail.com>
2026-02-24 11:34:36 +0200

    Init commit for backend

---
+++ backend/src/main/java/zw/gsu/smartassist/GsuSmartAssistApplication.java
@@ -0,0 +1,13 @@
+package zw.gsu.smartassist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsuSmartAssistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GsuSmartAssistApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/zw/gsu/smartassist/GsuSmartAssistApplicationTests.java
@@ -0,0 +1,13 @@
+package zw.gsu.smartassist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GsuSmartAssistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
