d1a41b7f21ba9664cd2971776dc5b1e3c5a6b2f5
manjozib <manjozib@gmail.com>
2026-02-25 18:51:30 +0200

    main app

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'app/modules/admin/dashboard/admin_dashboard_controller.dart';
+import 'app/modules/admin/login/admin_login_controller.dart';
 import 'app/modules/chat/chat_controller.dart';
 import 'app/routes/app_pages.dart';
 
 void main() {
   Get.put(ChatController());
+  Get.put(AdminLoginController());
+  Get.put(AdminManagementController());
   runApp(const MyApp());
 }
 
