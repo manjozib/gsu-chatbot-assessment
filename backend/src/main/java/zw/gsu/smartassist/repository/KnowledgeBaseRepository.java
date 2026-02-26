4b9b19a8f014cfcc1be48a06ba6bf49d123aef8c
manjozib <manjozib@gmail.com>
2026-02-26 16:34:40 +0200

    Query for searching answers from knowledge base using websearch_to_tsquery

--- backend/src/main/java/zw/gsu/smartassist/repository/KnowledgeBaseRepository.java
+++ backend/src/main/java/zw/gsu/smartassist/repository/KnowledgeBaseRepository.java
@@ -6,15 +6,55 @@ import org.springframework.data.repository.query.Param;
 import zw.gsu.smartassist.entity.KnowledgeBase;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface KnowledgeBaseRepository extends JpaRepository<KnowledgeBase, Long> {
 
-    @Query("""
-    SELECT k FROM KnowledgeBase k
-    WHERE lower(k.category) LIKE lower(concat('%', :q, '%'))
-       OR lower(k.question) LIKE lower(concat('%', :q, '%'))
-       OR lower(k.answer) LIKE lower(concat('%', :q, '%'))
-       OR lower(k.keywords) LIKE lower(concat('%', :q, '%'))
-    """)
-    List<KnowledgeBase> search(@Param("q") String q);
+//    @Query("""
+//    SELECT k FROM KnowledgeBase k
+//    WHERE lower(k.category) LIKE lower(concat('%', :q, '%'))
+//       OR lower(k.question) LIKE lower(concat('%', :q, '%'))
+//       OR lower(k.answer) LIKE lower(concat('%', :q, '%'))
+//       OR lower(k.keywords) LIKE lower(concat('%', :q, '%'))
+//    """)
+//    List<KnowledgeBase> search(@Param("q") String q);
+
+//    @Query(value = """
+//    SELECT *
+//    FROM knowledge_base k
+//    WHERE to_tsvector('english',
+//          coalesce(k.category,'') || ' ' ||
+//          coalesce(k.question,'') || ' ' ||
+//          coalesce(k.answer,'') || ' ' ||
+//          coalesce(k.keywords,'')
+//    )
+//    @@ plainto_tsquery('english', :searchText)
+//    """,
+//            nativeQuery = true)
+//    List<KnowledgeBase> search(@Param("searchText") String searchText);
+
+    @Query(value = """
+    SELECT *,
+           ts_rank(
+               to_tsvector('english',
+                   coalesce(category,'') || ' ' ||
+                   coalesce(question,'') || ' ' ||
+                   coalesce(answer,'') || ' ' ||
+                   coalesce(keywords,'')
+               ),
+               websearch_to_tsquery('english', :searchText)
+           ) AS rank
+    FROM knowledge_base
+    WHERE to_tsvector('english',
+            coalesce(category,'') || ' ' ||
+            coalesce(question,'') || ' ' ||
+            coalesce(answer,'') || ' ' ||
+            coalesce(keywords,'')
+          )
+          @@ websearch_to_tsquery('english', :searchText)
+    ORDER BY rank DESC
+    LIMIT 1
+    """,
+            nativeQuery = true)
+    List<KnowledgeBase> search(@Param("searchText") String searchText);
 }
