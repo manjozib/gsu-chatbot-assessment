c12d64cd6765fb99318cd8bfa2ebcb48106670e1
manjozib <manjozib@gmail.com>
2026-02-28 13:30:07 +0200

    adding FAQ route

--- frontend/lib/app/routes/app_pages.dart
+++ frontend/lib/app/routes/app_pages.dart
@@ -4,6 +4,7 @@ import '../modules/admin/dashboard/admin_dashboard_view.dart';
 import '../modules/chat/chat_view.dart';
 import '../modules/admin/login/admin_login_view.dart';
 import '../modules/config/server_config_view.dart';
+import '../modules/faq/faq_view.dart';
 
 class AppPages {
   static final routes = [
@@ -11,5 +12,6 @@ class AppPages {
     GetPage(name: '/admin/login', page: () => const AdminLoginView()),
     GetPage(name: '/admin/dashboard', page: () => const AdminDashboardView()),
     GetPage(name: '/config', page: () => const ServerConfigView()),
+    GetPage(name: '/faq', page: () => const FaqView()),
   ];
 }
